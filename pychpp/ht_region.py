15a3d3e37cddd823409ff7eb812c7d84f01d6c5c
Pierre Gobin <contact@pierregobin.fr>
2020-04-15 21:33:55 +0200

    Add HTRegion class

---
+++ pychpp/ht_region.py
@@ -0,0 +1,34 @@
+class HTRegion:
+    """
+    Hattrick region
+    """
+
+    _SOURCE_FILE = 'regiondetails'
+    _SOURCE_FILE_VERSION = '1.2'
+
+    def __init__(self, chpp, ht_id=None):
+
+        self._chpp = chpp
+        kwargs = {}
+
+        if ht_id is not None:
+            kwargs['arenaID'] = ht_id
+
+        data = chpp.request(file=self._SOURCE_FILE,
+                            version=self._SOURCE_FILE_VERSION,
+                            **kwargs,
+                            ).find('League')
+
+        region_data = data.find('Region')
+
+        self._data = data
+
+        self.ht_id = int(region_data.find('RegionID').text)
+        self.name = region_data.find('RegionName').text
+        self.number_of_users = int(region_data.find('NumberOfUsers'))
+        self.number_of_online = int(region_data.find('NumberOfOnline'))
+        self.weather = int(region_data.find('WeatherID'))
+        self.tomorrow_weather = int(region_data.find('TomorrowWeatherID'))
+
+    def __repr__(self):
+        return f'<{self.__class__.__name__} object : {self.name} ({self.ht_id})>'
