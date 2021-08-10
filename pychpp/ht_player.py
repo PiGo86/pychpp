761ca960bd6f5436ba0dcc6f91e21da97ab574bd
Pierre Gobin <dev@pierregobin.fr>
2021-08-10 23:14:02 +0200

    fix: remove age from HTLineupPlayer __repr__ special method

--- pychpp/ht_player.py
+++ pychpp/ht_player.py
@@ -304,6 +304,11 @@ class HTLineupPlayer(HTCorePlayer):
         super().__init__(**kwargs)
         self.is_youth = is_youth
 
+    def __str__(self):
+        # HTLineupPlayer object has no age attribute so that
+        # __str__() special method from HTCorePlayer has to be override
+        return super().__repr__()
+
     @property
     def role_name(self):
         role_names = {100: 'Keeper', 101: 'Right back',
