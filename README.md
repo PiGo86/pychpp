bb5b24f78f04eff7b5eb438afea2baf5ac8fc611
Pierre Gobin <p.dev@gobin.cc>
2024-10-27 21:30:33 +0100

    feat: add tournamentfixtures xml file support

--- README.md
+++ README.md
@@ -143,7 +143,7 @@ Then, to use this new class:
 In this way, only the data you're really interested in is parsed, which can in some cases be interesting from a performance point of view.
 
 ## List of supported CHPP XML files
-![46/57](https://progress-bar.xyz/81/?title=46%20on%2057)
+![47/57](https://progress-bar.xyz/82/?title=47%20on%2057)
 
 The following table shows the CHPP XML files that are currently supported:
 
@@ -188,6 +188,7 @@ The following table shows the CHPP XML files that are currently supported:
 |     Supporters      |     `supporters.xml`      |
 |     TeamDetails     |     `teamdetails.xml`     |
 |  TournamentDetails  |  `tournamentdetails.xml`  |
+| TournamentFixtures  | `tournamentfixtures.xml`  |
 |      Training       |      `training.xml`       |
 |    TransfersTeam    |    `transfersteam.xml`    |
 |      WorldCup       |      `worldcup.xml`       |
--- pychpp/chpp.py
+++ pychpp/chpp.py
@@ -16,7 +16,7 @@ from pychpp.models.xml import (manager_compendium, team_details, achievements, a
                                fans, hof_players, ladder_details, ladder_list, league_levels, live,
                                matches, match_orders, national_team_matches, national_players,
                                player_events, search, staff_avatars, staff_list, supporters,
-                               tournament_details)
+                               tournament_details, tournament_fixtures)
 from pychpp.models.custom import (ht_team, ht_arena, ht_user, ht_region, ht_youth_team, ht_player,
                                   ht_league_unit, ht_youth_player, ht_league, ht_matches_archive,
                                   ht_match, ht_challenge, ht_match_lineup, ht_transfer_history)
@@ -913,6 +913,14 @@ class CHPPXml(CHPPBase):
     ) -> tournament_details.TournamentDetails:
 
         return tournament_details.TournamentDetails(
+            chpp=self, tournament_id=tournament_id, season=season, **kwargs,
+        )
+
+    def xml_tournament_fixtures(
+            self, tournament_id: int, season: int = None, **kwargs,
+    ) -> tournament_fixtures.TournamentFixtures:
+
+        return tournament_fixtures.TournamentFixtures(
             chpp=self, tournament_id=tournament_id, season=season, **kwargs,
         )
 
