3182752ce2f1adbf3f7dc0b781ddef81df1e1a55
Pierre Gobin <p.dev@gobin.cc>
2025-11-22 17:16:02 +0100

    lint: unused imports

--- pychpp/models/custom/ht_youth_player.py
+++ pychpp/models/custom/ht_youth_player.py
@@ -1,6 +1,4 @@
 from pychpp.models.custom.base.ht_player import BaseCommonHTPlayer
-from pychpp.models.custom.base.ht_player import HTYouthPlayerSkills
-from pychpp.models.ht_field import HTProxyField
 from pychpp.models.xml.youth_player_details import YouthPlayerDetails
 
 
